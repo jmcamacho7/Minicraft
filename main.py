9450b578fff66f95c266919da8bfcc6683b865fd
GyroJMCS <gyrojmcs@gmail.com>
2022-07-14 01:40:34 +0200

    Bedrock y mapa infinito añadido

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ textura_piedra = load_texture('assets/piedra.jpg')
 textura_madera = load_texture('assets/madera.png')
 textura_lana = load_texture('assets/lana.png')
 textura_cielo = load_texture('assets/cielo.jpg')
+textura_bedrock = load_texture('assets/bedrock.png')
 textura_zombie = load_texture('assets/mobs/zombie/zombie.png')
 
 bloque = 1
@@ -23,6 +24,29 @@ def update():
     if held_keys['2']: bloque = 2
     if held_keys['3']: bloque = 3
     if held_keys['4']: bloque = 4
+    if held_keys['control']:
+        player.speed = 7
+    getTerr()
+
+class Bedrock(Button):
+    def __init__(self, position=(0,0,0), texture = textura_bedrock):
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'cube',
+            origin_y = 0.5,
+            texture = texture,
+            color = color.rgb(255,255,255),
+            highlight_color = color.gray,
+            shader = basic_lighting_shader,
+        )
+    def input(self,key):
+        if self.hovered:
+            if key == "left mouse down":
+                if bloque == 1: voxel = Voxel(position= self.position + mouse.normal, texture= textura_tierra)
+                if bloque == 2: voxel = Voxel(position= self.position + mouse.normal, texture= textura_piedra)
+                if bloque == 3: voxel = Voxel(position= self.position + mouse.normal, texture= textura_madera)
+                if bloque == 4: voxel = Voxel(position= self.position + mouse.normal, texture= textura_lana)
 
 
 class Voxel(Button):
@@ -60,19 +84,25 @@ class Cielo(Entity):
 			double_sided = True)
 
 
-chunkSize = 20
+chunkSize = 25
 
 semilla = random.randrange(1,1000000000)
 
 noise = PerlinNoise(octaves=3, seed=semilla)
-amp = random.randrange(1,6)
-freq = random.randrange(1,20)
+amp = random.randrange(1,10)
+freq = random.randrange(1,30)
+
+shells=[]
 
 for z in range(chunkSize*chunkSize):
-    cube = Voxel(texture=textura_tierra)
-    cube.x = floor(z/chunkSize)
-    cube.z = floor(z%chunkSize)
-    cube.y = floor(noise([cube.x/freq, cube.z/freq])*amp)
+    cube = Bedrock(texture=textura_bedrock)
+    shells.append(cube)
+    def getTerr():
+        global amp, freq
+        for i in range(len(shells)):
+            xx = shells[i].x = floor((i / chunkSize) + player.x - 0.5 * chunkSize)
+            zz = shells[i].z = floor((i % chunkSize) + player.z - 0.5 * chunkSize)
+            y = shells[i].y = floor(noise([xx / freq, zz / freq]) * amp)
 
 def input(key):
   if key == "escape":
