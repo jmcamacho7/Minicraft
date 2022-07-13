ef9f74aaabb0fe85fbf8ed50ca8f86b0a8ca4341
GyroJMCS <gyrojmcs@gmail.com>
2022-07-13 14:22:15 +0200

    Añadidos textura tierra y shaders

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import ursina.shader
 from ursina import *
 
 from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina.shaders import basic_lighting_shader
 
 app = Ursina()
+
+textura_tierra = load_texture('assets/tierra.jpg')
+textura_cielo = load_texture('assets/cielo.jpg')
 
 class Voxel(Button):
     def __init__(self, position=(0,0,0)):
@@ -11,9 +16,10 @@ class Voxel(Button):
             position = position,
             model = 'cube',
             origin_y = 0.5,
-            texture = 'grass',
+            texture = textura_tierra,
             color = color.rgb(255,255,255),
             highlight_color = color.lime,
+            shader = basic_lighting_shader,
         )
 
     def input(self,key):
@@ -23,7 +29,17 @@ class Voxel(Button):
             if key == "right mouse down":
                 destroy(self)
 
-chunkSize = 16
+
+class Cielo(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = scene,
+			model = 'sphere',
+			texture = textura_cielo,
+            scale = 250,
+			double_sided = True)
+
+chunkSize = 40
 
 for z in range(chunkSize):
     for x in range(chunkSize):
@@ -31,5 +47,7 @@ for z in range(chunkSize):
 
 
 player = FirstPersonController()
+
+cielo = Cielo()
 
 app.run()
