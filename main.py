f2caaff877084c67c215e0177d36a530aa14f78d
GyroJMCS <gyrojmcs@gmail.com>
2022-07-14 20:03:10 +0200

    a

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import basic_lighting_shader
 from perlin_noise import PerlinNoise
+from numpy import abs
 
 app = Ursina()
 
@@ -19,15 +20,18 @@ bloque = 1
 
 
 def update():
-    global bloque
+    global bloque, posicionxA, posicionzA
     if held_keys['1']: bloque = 1
     if held_keys['2']: bloque = 2
     if held_keys['3']: bloque = 3
     if held_keys['4']: bloque = 4
     if held_keys['control']:
         player.speed = 7
+    if      abs(player.z - posicionzA) > 1 or \
+            abs(player.x - posicionxA) > 1:
+        generarShell()
 
-class Bedrock(Button):
+class Bedrock(Entity):
     def __init__(self, position=(0,0,0), texture = textura_bedrock):
         super().__init__(
             parent = scene,
@@ -93,17 +97,46 @@ freq = random.randrange(3,30)
 
 
 terreno = Entity(model=None, collider=None)
+terrenowidth= 10
+subwidth= terrenowidth
+subsets = []
+subCubo = []
 
-for a in range(chunkSize*chunkSize):
+for i in range(subwidth):
     bud = Entity(model='cube')
-    bud.x = floor(a/chunkSize)
-    bud.z = floor(a%chunkSize)
-    bud.y = floor((noise([bud.x/freq,bud.z/freq]))*amp)
-    bud.parent = terreno
 
-terreno.combine()
-terreno.collider = 'mesh'
-terreno.texture= textura_tierra
+#for a in range(chunkSize*chunkSize):
+    # bud = Entity(model='cube')
+    # bud.x = floor(a/chunkSize)
+    # bud.z = floor(a%chunkSize)
+    # bud.y = floor((noise([bud.x/freq,bud.z/freq]))*amp)
+    # bud.parent = terreno
+
+# terreno.combine()
+# terreno.collider = 'mesh'
+# terreno.texture= textura_tierra
+
+shell = []
+shellWidth = 20
+
+for i in range(shellWidth*shellWidth):
+    blok = Entity(model='cube',
+                  collider='box',
+                  texture=textura_tierra,
+                  shader=basic_lighting_shader,
+                  highlight_color = color.gray,
+                  )
+
+    shell.append(blok)
+
+
+def generarShell():
+    global shellWidth, amp, freq
+    for i in range(len(shell)):
+        x = shell[i].x =  floor((i/shellWidth) + player.x - 0.5*shellWidth)
+        z = shell[i].z = floor((i%shellWidth) + player.z - 0.5*shellWidth)
+        y = shell[i].y = floor((noise([x/freq,z/freq]))*amp)
+
 
 window.show_ursina_splash=True
 
@@ -111,13 +144,18 @@ def input(key):
   if key == "escape":
     quit()
 
+scene.fog_color = color.rgb(0,200,211)
+scene.fog_density = 0.02
 
 player = FirstPersonController()
 player.x = chunkSize/2
 player.z= chunkSize/2
 player.gravity= 0.5
+posicionzA= player.z
+posicionxA= player.x
 
 cielo = Cielo()
+generarShell()
 
 
 
