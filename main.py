c909525e85e692cf0dd2fdebd62df9666df536f2
GyroJMCS <gyrojmcs@gmail.com>
2022-07-25 15:57:47 +0200

    Mundo generado infinito + musica

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import basic_lighting_shader
 from perlin_noise import PerlinNoise
 from nMap import nMap
+from numpy import *
 import time
 
 app = Ursina()
@@ -18,81 +19,32 @@ textura_bedrock = load_texture('assets/bedrock.png')
 textura_zombie = load_texture('assets/mobs/zombie/zombie.png')
 textura_frame = load_texture('assets/wireframe.png')
 
-musica = Audio(
+musica = []
+halland = Audio(
     'assets/Halland.mp3',
-    autoplay=True,
-    loop=True,
+    autoplay=False,
+    loop=False,
 )
-
-bloque = 1
-prevTime= time.time()
-
-bte = Entity(model='cube',texture=textura_frame)
-
-
-
-def herramientaConstruir():
-
-    bte.position = round(player.position +
-                         camera.forward * 3)
-    bte.y += 2
-    bte.y = round(bte.y)
-    bte.x = round(bte.x)
-    bte.z = round(bte.z)
-
-def construir():
-    c = duplicate(bte)
-    c.collider = 'cube'
-
-    if bloque == 1:
-        c.texture = textura_piedra
-
-    if bloque == 2:
-        c.texture = textura_madera
-
-    if bloque == 3:
-        c.texture = textura_tierra
-
-    if bloque == 4:
-        c.texture = textura_lana
-
-    c.shake(duration=0.5, speed=0.01)
-
-def input(key):
-
-    if key == 'control':
-        player.speed = 7
-
-    if key == "escape":
-        quit()
-
-    if key == 'left mouse up':
-        construir()
-
-    elif key == 'right mouse up':
-        x = mouse.hovered_entity
-        destroy(x)
+wet = Audio(
+    'assets/wet.mp3',
+    autoplay=False,
+    loop=False
+)
+sweden = Audio(
+    'assets/sweden.mp3',
+    autoplay=False,
+    loop=False
+)
+musica.append(halland)
+musica.append(wet)
+musica.append(sweden)
 
 
-def update():
-    global bloque, posicionxA, posicionzA, prevTime
-    if held_keys['1']: bloque = 1
-    if held_keys['2']: bloque = 2
-    if held_keys['3']: bloque = 3
-    if held_keys['4']: bloque = 4
-    if abs(player.z - posicionzA) > 1 or \
-            abs(player.x - posicionxA) > 1:
-        generarShell()
-    if time.time() - prevTime > 0.05:
-        generarSubset()
-        prevTime = time.time()
+def musicplayer ():
+    nombre = random.choice(musica)
+    nombre.play()
 
-    if player.y < -amp + 1:
-        player.y = 2 + floor((noise([player.x / freq,
-                                     player.z / freq])) * amp)
-        player.land()
-
-    herramientaConstruir()
+musicplayer()
 
 class Cielo(Entity):
     def __init__(self):
@@ -103,15 +55,133 @@ class Cielo(Entity):
             scale = 250,
 			double_sided = True)
 
+bloque = 1
+prevTime= time.time()
 
-semilla = random.randrange(1000, 2000000)
+objeto = Entity(model='cube',texture=textura_frame, shader=basic_lighting_shader)
 
-noise = PerlinNoise(octaves=1, seed=semilla)
-amp = random.randrange(20,70)
-freq = random.randrange(70, 100)
 
+class OTIPO:
+    PIEDRA = objeto.texture = textura_piedra
+    LANA = objeto.texture = textura_lana
+    TIERRA = objeto.texture = textura_tierra
+    MADERA = objeto.texture = textura_madera
+
+
+modoConstruccion = -1
+
+def seleccion():
+    if bloque == 1:
+        objeto.texture = OTIPO.TIERRA
+
+    if bloque == 2:
+        objeto.texture = OTIPO.PIEDRA
+
+    if bloque == 3:
+        objeto.texture = OTIPO.MADERA
+
+    if bloque == 4:
+        objeto.texture = OTIPO.LANA
+
+def herramientaConstruir():
+    if modoConstruccion  == -1:
+        objeto.visible = False
+        return
+
+    else: objeto.visible = True
+    objeto.position = round(player.position +
+                         camera.forward * 3)
+    objeto.y += 2
+    objeto.y = round(objeto.y)
+    objeto.x = round(objeto.x)
+    objeto.z = round(objeto.z)
+
+def construir():
+    seleccion()
+    c = duplicate(objeto)
+    c.collider = 'cube'
+    c.shake(duration=0.2, speed=0.01)
+
+
+
+def input(key):
+    global tipoBloque, modoConstruccion, generar, puedeGenerar
+    if key == "escape":
+        quit()
+
+    if key == 'g':
+        generar *= 1
+        puedeGenerar *= 1
+
+    if key == 'left mouse up':
+        construir()
+        objeto.texture=textura_frame
+
+    elif key == 'right mouse up':
+        x = mouse.hovered_entity
+        destroy(x)
+
+    if key == 'f' :
+        if modoConstruccion == 1:
+            modoConstruccion = -1
+        else:
+            modoConstruccion = 1,
+            objeto.texture = textura_frame
+
+
+def update():
+    global bloque, posicionxA, posicionzA, prevTime, rad, origen, generar, puedeGenerar
+
+    if held_keys['control']: player.speed = 7
+    else: player.speed = 4
+
+
+    if abs(player.z - posicionzA) > 1 or \
+            abs(player.x - posicionxA) > 1:
+            origen=player.position
+            rad=0
+            generar = 1 * puedeGenerar
+            posicionzA = player.z
+            posicionxA = player.x
+
+    generarShell()
+
+    if time.time() - prevTime > velocidadGen:
+        for x in range(porCiclo):
+            generarTerreno()
+        prevTime = time.time()
+
+    if held_keys['1']: bloque = 1
+    if held_keys['2']: bloque = 2
+    if held_keys['3']: bloque = 3
+    if held_keys['4']: bloque = 4
+
+    herramientaConstruir()
+
+
+
+
+noise = PerlinNoise(octaves=1, seed=99)
+amp = 0
+freq = 0
+
+generar = 1
+puedeGenerar = 1
+velocidadGen = 0
+porCiclo = 16
+cuboActual = 0
+subsetActual = 0
+numSubcubo = 16
+numSubsets = 420
+theta = 0
+rad = 0
+
+
+
+subDic = {}
 
 terreno = Entity(model=None, collider=None)
+
 chunkSize = 100
 subwidth= chunkSize
 subsets = []
@@ -121,70 +191,67 @@ sci = 0
 subsetActual = 0
 
 
-for i in range(subwidth):
+for x in range(numSubcubo):
     bud = Entity(model='cube')
+    bud.disable()
     subCubo.append(bud)
 
-for i in range(int((chunkSize*chunkSize)/subwidth)):
+for x in range(numSubsets):
     bud = Entity(model=None)
-    bud.parent = terreno
+    bud.texture = textura_bedrock
+    bud.disable()
     subsets.append(bud)
 
-def generarSubset():
-    global sci, subsetActual, noise, freq, amp
-    if subsetActual >= len(subsets):
-        terminarTerreno()
-        return
-    for i in range(subwidth):
-        x = subCubo[i].x = floor((i + sci)/chunkSize)
-        z = subCubo[i].z = floor((i + sci)%chunkSize)
-        y = subCubo[i].y = floor((noise([x/freq,z/freq]))*amp)
+def perlinGen(_x, _z):
+    y = 0
+    freq = 64
+    amp = 42
+    y += ((noise([_x/freq,_z/freq]))*amp)
+    freq = 32
+    amp = 21
+    y += ((noise([_x/freq,_z/freq]))*amp)
+    return floor(y)
 
-        subCubo[i].parent = subsets[subsetActual]
-        subCubo[i].visible = False
+def generarTerreno():
+    global cuboActual, theta, rad, subsetActual
+    global generar
 
-    y += random.randrange(-4,4)
-    r = 0
-    g = 0
-    b = 0
-    if y > amp * 0.3:
-        b = 255
-    if y == 4:
-        r = g = b = 255
+    if generar == -1: return
+
+    # Decide where to place new terrain cube!
+    x = floor(origen.x + sin(radians(theta)) * rad)
+    z = floor(origen.z + cos(radians(theta)) * rad)
+    # Check whether there is terrain here already...
+    if subDic.get('x' + str(x) + 'z' + str(z)) != 'i':
+        subCubo[cuboActual].enable()
+        subCubo[cuboActual].x = x
+        subCubo[cuboActual].z = z
+        subDic['x' + str(x) + 'z' + str(z)] = 'i'
+        subCubo[cuboActual].parent = subsets[subsetActual]
+        subCubo[cuboActual].y = perlinGen(x, z)
+        subCubo[cuboActual].disable()
+        cuboActual += 1
+
+        if cuboActual == numSubcubo:
+            subsets[subsetActual].combine(auto_destroy=False)
+            subsets[subsetActual].enable()
+            subsetActual += 1
+            cuboActual = 0
+
     else:
-        g = nMap(y, 0, amp * 0.5, 0, 255)
-    subCubo[i].color = color.rgb(r, g, b)
-    subCubo[i].visible = False
+        pass
+        # There was terrain already there, so
+        # continue rotation to find new terrain spot.
 
-    subsets[subsetActual].combine(auto_destroy=False)
-    sci += subwidth
-    subsetActual += 1
+    if rad > 0:
+        theta += 45 / rad
+    else:
+        rad += 0.5
 
+    if theta >= 360:
+        theta = 0
+        rad += 0.5
 
-terrenoAcabado = False
-
-def terminarTerreno():
-    global terrenoAcabado
-    if terrenoAcabado == True: return
-    terreno.combine()
-    terrenoAcabado = True
-    terreno.texture = textura_bedrock
-
-
-
-
-
-
-#for a in range(chunkSize*chunkSize):
-    # bud = Entity(model='cube')
-    # bud.x = floor(a/chunkSize)
-    # bud.z = floor(a%chunkSize)
-    # bud.y = floor((noise([bud.x/freq,bud.z/freq]))*amp)
-    # bud.parent = terreno
-
-# terreno.combine()
-# terreno.collider = 'mesh'
-# terreno.texture= textura_tierra
 
 shell = []
 shellWidth = 3
@@ -198,13 +265,13 @@ for i in range(shellWidth*shellWidth):
 
 
 def generarShell():
-    global shellWidth, amp, freq
+    global shellWidth
     for i in range(len(shell)):
         x = shell[i].x =  floor((i/shellWidth) +
                                 player.x - 0.5*shellWidth)
         z = shell[i].z = floor((i%shellWidth) +
                                player.z - 0.5*shellWidth)
-        y = shell[i].y = floor((noise([x/freq,z/freq]))*amp)
+        shell[i].y = perlinGen(x,z)
 
 
 
@@ -218,6 +285,7 @@ player.z= chunkSize/2
 player.gravity= 0.5
 posicionzA= player.z
 posicionxA= player.x
+origen = player.position
 
 construir()
 
